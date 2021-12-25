2ce72a173acca076e7790fc255ad23c2431db9df
MaximSerebryany <serebryanymax@gmail.com>
2021-12-25 14:48:51 +0300

    цвета

---
+++ BackgroundColor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
